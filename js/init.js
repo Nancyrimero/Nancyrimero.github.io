51339ebfa1f2c81ec051b7d7579716422c26dab0
Nancyrimero <82417359+Nancyrimero@users.noreply.github.com>
2021-04-12 16:31:28 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCWMNVEBuinCUlYvyMTy64a-8tsWckZWXw",
+    authDomain: "chatnacy41m.firebaseapp.com",
+    projectId: "chatnacy41m",
+    storageBucket: "chatnacy41m.appspot.com",
+    messagingSenderId: "560439324509",
+    appId: "1:560439324509:web:43bd8efc09599d7373c2b4",
+    measurementId: "G-NNVKFQ0TRS"
+});
